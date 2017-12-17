fd0914faafbc62662ff0ef29e227a51911d4bfee
Holman Gao <holman@golmansax.com>
2017-12-17 15:33:55 -0500

    Use spawn to log stdout and stderr

--- src/utils.ts
+++ src/utils.ts
@@ -8,18 +8,41 @@
      ## ## ## :##
       ## ## ##*/
 
-import { exec as execAsync, ExecOptions } from 'child_process'
+import { spawn, ExecOptions } from 'child_process'
 import { readFile as readFileAsync, writeFile as writeFileAsync } from 'fs'
 import { dir as tmpDir } from 'tmp'
 
 export const exec = (command: string, options: ExecOptions) =>
-  new Promise<{ stdout: string; stderr: string }>((resolve, reject) =>
-    execAsync(
-      command,
-      options,
-      (err, stdout, stderr) => (err ? reject(err) : resolve({ stdout, stderr }))
-    )
-  )
+  new Promise<{ stdout: string; stderr: string }>((resolve, reject) => {
+
+    const args = command.split(' ');
+    const instance = spawn(args[0], args.slice(1), options);
+
+    let stdout = '';
+    let stderr = '';
+
+    instance.stdout.on('data', (data) => {
+      const dataStr = data.toString();
+      console.log('stdout:', dataStr);
+      stdout += dataStr;
+    });
+
+    instance.stderr.on('data', (data) => {
+      const dataStr = data.toString();
+      console.log('stderr:', dataStr);
+      stderr += dataStr;
+    });
+
+    instance.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Child process exited with code ${code} and stderr output: ${stderr}`));
+        return;
+      }
+
+      console.log('child process exited with code:', code);
+      resolve({ stdout, stderr });
+    });
+  })
 
 export const readFile = (path: string) =>
   new Promise<Buffer>((resolve, reject) =>
